1c10ae56c7d3eb27dc64530ce391d985ab8963fc
Anjumol Tennyson <anjutennyson@gmail.com>
2020-04-21 15:27:12 +0530

    all static files are added

--- wastemanagement/user/views.py
+++ wastemanagement/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
-from new.models import AddProduct
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
