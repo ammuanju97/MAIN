2ca2ff0d84d1b30022eb4687d9f9740523e9ec88
Anjumol Tennyson <anjutennyson@gmail.com>
2020-05-17 13:13:14 +0530

    models of common

--- wastemanagement/common/models.py
+++ wastemanagement/common/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User,auth
 
 # Create your models here.
+class AddCategory(models.Model):
+    categoryname = models.CharField(max_length=100)
+ 
+
+    def __str__(self):
+        return self.categoryname
+
+class AddRecycle(models.Model):
+    categoryname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.categoryname
