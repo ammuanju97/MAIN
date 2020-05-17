1cee246fa6494e9855b3826535d4caa6dfab71b1
Anjumol Tennyson <anjutennyson@gmail.com>
2020-05-17 13:14:41 +0530

    views of common

--- wastemanagement/common/views.py
+++ wastemanagement/common/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from common.models import AddCategory, AddRecycle
+from django.http import HttpResponse
+from django.contrib import messages
+from user.models import UserRegister, CollectRegister1
 # Create your views here.
+def category(request):
+    return render(request, 'category.html')
+
+def adminhome(request):
+    return render(request, 'adminhome.html')
+
+def addcategory(request):
+    if request.method == 'POST':
+        categoryname = request.POST['categoryname']
+        
+        addcat = AddCategory(categoryname=categoryname)
+        addcat.save()
+        return render(request, 'category.html')
+
+def category1(request):
+    return render(request, 'category1.html')
+
+def addrecycle(request):
+    if request.method == 'POST':
+        categoryname = request.POST['categoryname']
+        addre = AddRecycle(categoryname=categoryname)
+        addre.save()
+        return render(request, 'category1.html')
+
+
+def adminhome1(request):
+    results=request.GET["typeofusers"] 
+    return render(request, 'adminhome1.html',{'typeofusers':results})
+
+
+def addadmin(request):
+    return render(request, 'addadmin.html')
+
+
+def viewuserlist(request):
+    u1=UserRegister.objects.filter()
+    return render(request, 'viewuserlist.html', {'u1':u1})
+
+def viewcollect(request):
+    v1=CollectRegister1.objects.filter()
+    return render(request, 'viewcollect.html',{'v1':v1})
+
+def approvcollect(request):
+    v2=CollectRegister1.objects.filter()
+    return render(request, 'approvcollect.html',{'v2':v2})
+
+
+
