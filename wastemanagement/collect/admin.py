74c4b552b8f9ceb7056e6f4b53b9a9aafa810036
Anjumol Tennyson <anjutennyson@gmail.com>
2020-05-17 13:01:47 +0530

    upadte admin.py of collect app

--- wastemanagement/collect/admin.py
+++ wastemanagement/collect/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import AddProduct, CollectDetails
 # Register your models here.
+
+
+# from django.contrib import admin
+# from .models import AddProduct
+# Register your models here.
+admin.site.register(AddProduct)
+
+admin.site.register(CollectDetails)
