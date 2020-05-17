65a03fcd4978ba7a9515a828479638a6ce4fd316
Anjumol Tennyson <anjutennyson@gmail.com>
2020-05-17 12:59:04 +0530

    app added to wastemanagement

--- wastemanagement/wastemanagement/urls.py
+++ wastemanagement/wastemanagement/urls.py
@@ -22,4 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('', include('common.urls')),
+    path('', include('collect.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
