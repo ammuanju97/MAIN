c079e45fc4a01f5f931a11d60dbb870ede296984
Anjumol Tennyson <anjutennyson@gmail.com>
2020-05-17 13:16:35 +0530

    admin.py of user

--- wastemanagement/user/admin.py
+++ wastemanagement/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import UserRegister, CollectRegister1, Sell
 # Register your models here.
+
+admin.site.register(UserRegister)
+
+admin.site.register(CollectRegister1)
+
+
+
+admin.site.register(Sell)
+
