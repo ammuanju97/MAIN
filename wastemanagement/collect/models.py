5d94d0b81f1efb8ff52f2ac19dff121409347cad
Anjumol Tennyson <anjutennyson@gmail.com>
2020-08-08 12:49:22 +0530

    models uppdated

--- wastemanagement/collect/models.py
+++ wastemanagement/collect/models.py
@@ -28,3 +28,32 @@ class CollectDetails(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class BookForm(models.Model):
+    tokenno = models.CharField(max_length=100)
+    cname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    wastetype = models.CharField(max_length=100)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.cname
+
+class Payment(models.Model):
+    cardnumber = models.IntegerField()
+    expirydate = models.DateField()
+    cvv = models.IntegerField()
+    nameofcard = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nameofcard
+
+class SellPayment(models.Model):
+    cardnumber = models.IntegerField()
+    expirydate = models.DateField()
+    cvv = models.IntegerField()
+    nameofcard = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nameofcard
